1fe0b75304424f4ffd28bb3a75f29be7c1604f2d
ahsalem <ahsalem@ahsalem.myguest.virtualbox.org>
2023-03-10 13:59:09 +0400

    after fixing mira

--- requirments/srcs/requirments/mariadb/tools/entry.sh
+++ requirments/srcs/requirments/mariadb/tools/entry.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-echo "Starting the mysql service!"
-service mysql start 
-sleep 5
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; \
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; \
-	GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; \
-	FLUSH PRIVILEGES;"
+# apt-get install procps -y
+# # echo "Starting the mysql service!"
+# service mysql start 
+mysqld_safe &
+sleep 10
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; \
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; \
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; \
+FLUSH PRIVILEGES;"
 
+
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e ";"
 # GRANT CREATE USER ON *.* TO 'root'@'localhost'; \
-sleep 5
+# sleep infinity
 echo Running the mysql service!
 echo admin $MYSQL_USER should created, if you see database created, you are good to go!
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;"
-mysqld --bind-address=0.0.0.0 
+# mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;"
+# # mysql stop
+# sleep infinity
+mysqld_safe  --bind-address=0.0.0.0 
+
+# RUN mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}; \
+# 	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
+# 	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
+# 	FLUSH PRIVILEGES;"
